61fb8356d2eee862dddb3eeb8e1680f57e28848e
Sabina Shrestha <sabina.pari@yahoo.com>
2015-08-04 01:29:28 -0600

    first commit

---
+++ app/views/welcome/welcome.js
@@ -0,0 +1,22 @@
+ $(document).ready(function(){
+//     $('section[data-type="background"]').each(function(){
+//         var $bgobj = $(this); // assigning the object
+    
+//         $(window).scroll(function() {
+//             var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+            
+//             // Put together our final background position
+//             var coords = '50% '+ yPos + 'px';
+
+//             // Move the background
+//             $bgobj.css({ backgroundPosition: coords });
+//         }); 
+//     }); 
+function parallax(){
+    var prlx_lyr_1 = document.getElementById('prlx_lyr_1');
+    prlx_lyr_1.style.top = -(window.pageYOffset / 4) + 'px';
+}
+
+window.addEventListener("scroll", parallax, false);   
+ });
+
